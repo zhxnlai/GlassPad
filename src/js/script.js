99676f729807f66f6292f756e593965823c25d74
Zhixuan Lai <zhxnlai@gmail.com>
2014-11-21 19:25:30 -0800

    add src folder

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+//    $('.single-item').slick({
+//      dots: true,
+//      speed: 500
+//    });
+//    
+//
+//      $("#owl-demo").owlCarousel({
+//     nav:true,
+//
+//      navigation : true, // Show next and prev buttons
+//      slideSpeed : 300,
+//      paginationSpeed : 400,
+//      singleItem:true,
+//     items:1,
+//    autoplay:true,
+//    autoplayTimeout:1000,
+//    autoplayHoverPause:true
+//
+// 
+//  });
+    
+//    alert(document.URL);
+
+
+});
+
---
+++ src/react.js
@@ -0,0 +1,273 @@
+var NavItem = React.createClass({
+    getInitialState: function() {
+        return {selected: false};
+    },
+    render: function() {
+        return (
+            <div className={this.props.selected ? "pure-menu-selected" : ""}><a >{this.props.title}</a></div>
+        )
+    }
+});
+var Header = React.createClass({
+    handleClick: function(i) {
+          this.props.onClick(this, i);
+    },
+    render: function() {
+        var navItems = this.props.pages.map(function(pageName, i) {
+            return (
+                <li name={pageName} onClick={this.handleClick.bind(this, i)}><NavItem key={i} title={pageName} selected={i==this.props.pageIndex}/></li>
+                )
+        }, this)
+        return (
+            <div className="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+                <a className="pure-menu-heading" href="/">GlassPad</a>
+                <ul >{navItems}</ul>
+            </div>
+        );
+    }
+});
+// footer
+var Footer = React.createClass({
+    render: function() {
+        return (<footer className="footer l-box is-center">Copyright © 2054 GlassPad Inc. All Rights Reserved.</footer>
+        );
+    }
+});
+// bullet point list
+
+// splash
+var Splash = React.createClass({
+    render: function() {
+        return <div className="splash-container">{this.props.content}</div>
+    }
+})
+var ImageCarousel = React.createClass({
+    render: function() {
+//        console.log("images" + self.props.urls)
+        var images = (this.props.urls.map(function(url) {return (                  <div className="item"><img className="pure-img" src={url}/></div>)}, this))
+        return ( <div className="splash-owl">{images}</div>
+        )}
+})
+var ContentWrapper = React.createClass({
+    render: function() {
+        return <div className="content-wrapper">{this.props.content}</div>
+    }
+})
+var ContentWrapperOther = React.createClass({
+    render: function() {
+        return <div className="content-wrapper-other">{this.props.content}</div>
+    }
+})
+var BulletPoints = React.createClass({
+    render: function() {
+        var bulletPointNodes = this.props.points.map(function(point) {return (<div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 className="content-subhead">
+                    <i className="fa fa-rocket"></i>
+                    {point.title}
+                </h3>
+                <p> {point.detail}
+                </p></div>)}, this)
+        return (<div class="content">
+        <h2 className="content-head is-center">{this.props.header}</h2>
+                {bulletPointNodes}
+               </div>)
+    }
+})
+var Features = React.createClass({
+    render: function() {
+        var featureNodes = this.props.points.map(function(point) {return (<div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+        <h2 className="content-head is-center">{point.title}</h2>
+        <div className="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
+        <img className="pure-img-responsive" alt="File Icons" width="300" src="img/common/file-icons.png"/>
+        </div>
+<div className="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+                <p> {point.detail}
+                </p></div></div>)}, this)
+        return (<div className="content">
+                {featureNodes}
+               </div>)
+    }
+})
+// bio in markdown format
+
+// feature banner
+
+
+
+var PageNotFound = React.createClass({
+  render: function() {
+    return (
+        <h1>Page not Found</h1>
+    );
+  }
+});
+
+
+var imageURLs = ["resources/tablet-landscape.png"]
+var bulletPointsHeader = "aljs;dfjk;we"
+var bulletPoints = [{"i": "fa fa-rocket", "title": "best product", "detail": "ials;dfjl;awjef"}]
+var Home = React.createClass({
+  render: function() {
+    return (
+    <div>
+    <Splash content={<ImageCarousel urls={imageURLs}/>}/>
+    <ContentWrapper content={<div>
+        <BulletPoints header={bulletPointsHeader} points={bulletPoints}/>
+        <Footer />
+        </div>}/>
+        </div>
+    );
+  }
+});
+
+var features = [{title:"scans", url:"", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit,"}]
+var Features = React.createClass({
+  render: function() {
+    return (
+    <div>
+    <ContentWrapperOther content={
+        <div>
+        <Features points={features}/>
+        <Footer /></div>}
+        />
+        </div>
+    );
+  }
+});
+
+var AjaxSectionWrapper = React.createClass({
+  getInitialState: function() {
+    return {content: <div />};
+  },
+  componentDidMount: function() {
+      $.ajax({
+      url: this.props.url,
+      dataType: 'html',
+      success: function(data) {
+        this.setState({content: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+    render: function() {
+      return (
+              <section name={this.props.sectionName} dangerouslySetInnerHTML={{__html: this.state.content}} />
+
+      )}
+
+})
+
+var Buy_now = React.createClass({
+    render: function() {
+        return (
+        <div>
+          <ContentWrapperOther content={
+            <div className="content">
+      				<h2 className="content-head is-center">Sign up for the latest updates.</h2>
+                <div className="pure-g">
+                    <div className="l-box-lrg pure-u-1-2 pure-u-md-2-5">
+                        <img className="pure-img" src="resources/tablet-home.png" />
+                    </div>
+          					<div className="l-box-lrg pure-u-1-2 pure-u-md-2-5">
+          						<form className="pure-form pure-form-stacked">
+          							<fieldset>
+          								<label for="name">Name</label>
+          								<input id="name" type="text" placeholder="Your Name"/>
+          								<label for="email">Email</label>
+          								<input id="email" type="email" placeholder="Your Email"/>
+          								<button type="submit" class="pure-button">Keep me informed</button>
+          							</fieldset>
+          						</form>
+          					</div>
+
+      				</div>
+      			</div>
+            } />
+        </div>
+
+        )
+    }
+});
+
+
+var Router = React.createClass({
+    getInitialState: function() {
+        return {pageIndex:0, pageHtml:this.props.pageHtmls[0], component: <div />};
+    },
+  componentDidMount: function() {
+    var scrollTop = $(document).scrollTop()
+    var accHeight = 0;
+    console.log("scroll Top: "+scrollTop)
+    for (var i=0;i<this.props.pages.length;i++) {
+      var name = this.props.pages[i]
+      accHeight = accHeight+ $("section[name='"+name+"']").height()
+      if (scrollTop<=accHeight) {
+        // console.log("name "+name)
+        this.setState({pageIndex:i})
+        break;
+      }
+    }
+
+  },
+
+  handleHeaderClick: function(o, i) {
+    console.log("click: "+i + ' html: '+this.props.pageHtmls[i])
+    // page(i==0 ? '/':'/'+this.props.pages[i])
+// this.setState({pageIndex: i, component: <AjaxContentWrapper url={this.props.pageHtmls[i]}/>})
+    this.setState({pageIndex: i, pageHtml: this.props.pageHtmls[i]})
+
+    $("html, body").animate({
+        scrollTop: $("section[name='"+this.props.pages[i]+"']").offset().top
+    });
+
+
+  },
+  handleScroll: function(e) {
+    var scrollTop = $(document).scrollTop()
+    var accHeight = 0;
+    for (var i=0;i<this.props.pages.length;i++) {
+      var name = this.props.pages[i]
+      accHeight = accHeight+ $("section[name='"+name+"']").height()
+      if (scrollTop<=accHeight) {
+        this.setState({pageIndex:i})
+        break;
+      }
+    }
+  },
+  render: function () {
+    var contents = this.props.pageHtmls.map(function(pageHtml, i) {
+      return (<div>
+      <AjaxSectionWrapper sectionName={this.props.pages[i]} url={"/components/"+pageHtml}/>
+      </div>)
+    },this)
+    return (<div>
+            <Header pages={this.props.pages} pageIndex={this.state.pageIndex} onClick={this.handleHeaderClick}/>
+            <ContentWrapperOther ref="content" content={
+                <div onWheel={this.handleScroll}>
+                {contents}
+                <Footer /></div>} />
+
+            </div>)
+  }
+});
+
+//<hr className="style-two"/>
+
+
+var pages = ["Home", "Design", "Features", "Specs", "History", "About", "Buy-Now"]
+var pageHtmls = ["home.html", "design.html", "features.html", "specs.html", "history.html", "about.html", "buy_now.html"]
+React.render(<Router pages={pages} pageHtmls={pageHtmls}/>, document.querySelector('body'));
+
+// $(document).ready(function(){
+//   for (var i=0;i<pages.length; i++) {
+//
+//   $("section[name='"+pages[i]+"']").waypoint(function() {
+//     var name = $(this).attr("name")
+//     console.log('Basic example callback triggered.'+$(this).attr("name"))
+//     // $('li[name='+name+']').click();
+//   });
+// }
+
+// });
